a002adf70a7062cba900e26c6f4518f0f8f7841f
Sbrubles08 <92830961+Sbrubles08@users.noreply.github.com>
2022-11-30 20:55:18 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+   apiKey: "AIzaSyCZltV8jOrzWY0BRP_A4EklZA3ageN52n0",
+  authDomain: "projeto-c71-e8405.firebaseapp.com",
+  projectId: "projeto-c71-e8405",
+  storageBucket: "projeto-c71-e8405.appspot.com",
+  messagingSenderId: "708928116167",
+  appId: "1:708928116167:web:b443c358f932b289e3f5e2" 
 };
 
 firebase.initializeApp(firebaseConfig);
